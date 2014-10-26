534546f8fd2de80f876f9f3168866f3e636764ef
MURAOKA Yusuke <yusuke@jbking.org>
2014-10-26 20:15:06 +0900

    refactor tests for ImageField

--- tests/fields/image_field.py
+++ tests/fields/image_field.py
@@ -53,7 +53,29 @@ class ImageFieldTestCase(BaseTestCase):
         dj_obj.image.open()
         self.assertEqual(dj_obj.image.read(6), 'GIF87a')
 
-    def test_with_django_dimension_fields(self):
+    def test_without_django(self):
+        from djangostdnet import models
+
+        class AModel(models.Model):
+            image = models.ImageField(upload_to='image_field_test',
+                                      required=False)
+
+            class Meta:
+                register = False
+
+        image = self.allocateImage()
+        obj = AModel.objects.new(image=image)
+        self.addCleanup(self.cleanupImage, obj.image)
+        self.addCleanup(self.cleanupStorage, obj.image.field.storage, 'image_field_test')
+
+        self.assertEqual(obj.image.name, 'image_field_test/test.gif')
+
+        obj = AModel.objects.get(id=obj.id)
+
+        obj.image.open()
+        self.assertEqual(obj.image.read(6), 'GIF87a')
+
+    def test_dimension_fields_with_django(self):
         from django.db import models as dj_models
         from djangostdnet import models
 
@@ -78,18 +100,15 @@ class ImageFieldTestCase(BaseTestCase):
 
         dj_obj = ADjangoModel.objects.get(pk=obj.id)
 
-        dj_obj.image.open()
-        self.assertEqual(dj_obj.image.read(6), 'GIF87a')
         self.assertEqual(dj_obj.width, 10)
         self.assertEqual(dj_obj.height, 20)
 
-    def test_without_django(self):
-        import os
+    def test_dimension_fields_without_django(self):
         from stdnet import odm
         from djangostdnet import models
 
         class AModel(models.Model):
-            image = models.ImageField(width_field='width', height_field='height', upload_to='image_field_test',
+            image = models.ImageField(width_field='width', height_field='height',
                                       required=False)
             width = odm.IntegerField(required=False)
             height = odm.IntegerField(required=False)
@@ -100,23 +119,14 @@ class ImageFieldTestCase(BaseTestCase):
         image = self.allocateImage()
         obj = AModel.objects.new(image=image)
         self.addCleanup(self.cleanupImage, obj.image)
-        self.addCleanup(self.cleanupStorage, obj.image.field.storage, 'image_field_test')
 
         self.assertEqual(obj.width, 10)
         self.assertEqual(obj.height, 20)
-        self.assertEqual(obj.image.name, 'image_field_test/test.gif')
 
         obj = AModel.objects.get(id=obj.id)
 
-        obj.image.open()
-        self.assertEqual(obj.image.read(6), 'GIF87a')
         self.assertEqual(obj.width, 10)
         self.assertEqual(obj.height, 20)
-
-        image_path = obj.image.path
-        obj.image.delete()
-        obj.delete()
-        self.assertFalse(os.path.isfile(image_path))
 
     def test_delete_from_django(self):
         import os
@@ -186,7 +196,6 @@ class ImageFieldTestCase(BaseTestCase):
             height = dj_models.IntegerField(null=True)
 
         class AModel(models.Model):
-
             class Meta:
                 register = False
                 django_model = ADjangoModel
@@ -198,14 +207,14 @@ class ImageFieldTestCase(BaseTestCase):
         self.assertFalse(dj_obj.image)
         self.assertFalse(obj.image)
 
-    def test_upload_to_func_with_django(self):
+    def test_func_upload_to_with_django(self):
         import os
         from django.conf import settings
         from django.db import models as dj_models
         from djangostdnet import models
 
         def gen_custom_path(instance, filename):
-            return 'custom_path/%s' % filename
+            return os.path.join('custom_path', filename)
 
         class ADjangoModel(dj_models.Model):
             image = dj_models.ImageField(upload_to=gen_custom_path)
@@ -223,17 +232,18 @@ class ImageFieldTestCase(BaseTestCase):
 
         dj_obj = ADjangoModel.objects.get(pk=obj.id)
 
-        self.assertTrue(obj.image.path.startswith(os.path.join(settings.MEDIA_ROOT, 'custom_path')))
-        self.assertTrue(dj_obj.image.path.startswith(os.path.join(settings.MEDIA_ROOT, 'custom_path')))
+        custom_path_prefix = os.path.join(settings.MEDIA_ROOT, 'custom_path')
+        self.assertTrue(obj.image.path.startswith(custom_path_prefix))
+        self.assertTrue(dj_obj.image.path.startswith(custom_path_prefix))
         self.assertEqual(obj.image.path, dj_obj.image.path)
 
-    def test_upload_to_func_without_django(self):
+    def test_func_upload_to_without_django(self):
         import os
         from django.conf import settings
         from djangostdnet import models
 
         def gen_custom_path(instance, filename):
-            return 'custom_path/%s' % filename
+            return os.path.join('custom_path', filename)
 
         class AModel(models.Model):
             image = models.ImageField(upload_to=gen_custom_path)
@@ -245,4 +255,54 @@ class ImageFieldTestCase(BaseTestCase):
         obj = AModel.objects.new(image=image)
         self.addCleanup(self.cleanupImage, obj.image)
 
-        self.assertTrue(obj.image.path.startswith(os.path.join(settings.MEDIA_ROOT, 'custom_path')))
+        custom_path_prefix = os.path.join(settings.MEDIA_ROOT, 'custom_path')
+        self.assertTrue(obj.image.path.startswith(custom_path_prefix))
+
+    def test_strftime_upload_to_with_django(self):
+        import os
+        from freezegun import freeze_time
+        from django.conf import settings
+        from django.db import models as dj_models
+        from djangostdnet import models
+
+        class ADjangoModel(dj_models.Model):
+            image = dj_models.ImageField(upload_to='custom_path/%Y%m%d')
+
+        class AModel(models.Model):
+            class Meta:
+                django_model = ADjangoModel
+                register = False
+
+        self.create_table_for_model(ADjangoModel)
+
+        image = self.allocateImage()
+        with freeze_time('1990-01-01'):
+            obj = AModel.objects.new(image=image)
+        self.addCleanup(self.cleanupImage, obj.image)
+
+        dj_obj = ADjangoModel.objects.get(pk=obj.id)
+
+        custom_path_prefix = os.path.join(settings.MEDIA_ROOT, 'custom_path', '19900101')
+        self.assertTrue(obj.image.path.startswith(custom_path_prefix))
+        self.assertTrue(dj_obj.image.path.startswith(custom_path_prefix))
+        self.assertEqual(obj.image.path, dj_obj.image.path)
+
+    def test_strftime_upload_to_without_django(self):
+        import os
+        from freezegun import freeze_time
+        from django.conf import settings
+        from djangostdnet import models
+
+        class AModel(models.Model):
+            image = models.ImageField(upload_to='custom_path/%Y%m%d')
+
+            class Meta:
+                register = False
+
+        image = self.allocateImage()
+        with freeze_time('1990-01-01'):
+            obj = AModel.objects.new(image=image)
+        self.addCleanup(self.cleanupImage, obj.image)
+
+        custom_path_prefix = os.path.join(settings.MEDIA_ROOT, 'custom_path', '19900101')
+        self.assertTrue(obj.image.path.startswith(custom_path_prefix))
